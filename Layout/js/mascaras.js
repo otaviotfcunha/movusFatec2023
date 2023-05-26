960be4be6db2523c4c0cc24fba97517598e13804
luan <luangarciaoficial@gmail.com>
2023-05-25 23:40:48 -0300

    validação login

--- Layout/js/mascaras.js
+++ Layout/js/mascaras.js
@@ -1,3 +1,40 @@
+//validação de campos
+
+const form = document.getElementById('formLogin');
+const campos = document.querySelectorAll('.required');
+const spans = document.querySelectorAll('.span-required');
+
+function setError(index){
+    campos[index].style.border = '3px solid #B2292C';
+    spans[index].style.display = 'block';
+}
+
+function removeError(index) {
+    campos[index].style.border = '';
+    spans[index].style.display = 'none';
+}
+
+function nameValidate() {
+    if(campos[0].value.length < 3) {
+        setError(0);
+    }
+    else {
+        removeError(0);
+    }
+}
+
+function passValidate() {
+    if(campos[1].value.length < 8) {
+        setError(1);
+    }
+    else {
+        removeError(1);
+    }
+}
+
+//mascaras
+
+
 function mascaraCPF(){
     const input = document.getElementById("CPF")
     
